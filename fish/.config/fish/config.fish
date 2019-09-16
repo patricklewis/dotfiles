65ef6ce4629b2a7677ca049973792b272e11c369
Patrick Lewis <patrick@patricklewis.org>
2019-09-16 09:59:59 -0400

    Update fish configuration to install fisher

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -39,3 +39,9 @@ if test -d ~/.nodenv/shims
   status --is-interactive; and source (nodenv init - --no-rehash|psub)
 end
 
+# bootstrap fisher
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
