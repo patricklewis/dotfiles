3f85f84987a5ce9eaa6f0ab10af243c040005d95
Patrick Lewis <patrick@patricklewis.org>
2023-02-20 09:43:53 -0500

    Add/configure plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,27 +1,72 @@
 return {
 	{
-		"ggandor/leap.nvim",
+		'ggandor/leap.nvim',
 		config = function()
 			require('leap').add_default_mappings()
 		end
 	},
+  {
+    'ibhagwan/fzf-lua',
+    keys = {
+      {
+        '<leader>ff',
+        function()
+          require("fzf-lua").files()
+        end
+      },
+      {
+        '<leader>fb',
+        function()
+          require("fzf-lua").buffers()
+        end
+      }
+    }
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
 	{
-		"glepnir/dashboard-nvim",
-		event = 'VimEnter',
-		config = function()
-			require('dashboard').setup {
-				-- config
-			}
-		end,
-		dependencies = { { 'nvim-tree/nvim-web-devicons' } }
-	},
-	{
-		"Mofiqul/dracula.nvim",
+		'Mofiqul/dracula.nvim',
+    lazy = false,
+    priority = 1000,
 		config = function()
 			vim.cmd[[colorscheme dracula]]
 		end
 	},
+  {
+    'ntpeters/vim-better-whitespace'
+  },
 	{
-		"nvim-lualine/lualine.nvim"
+		'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'dracula'
+        }
+      }
+    end
+	},
+	{
+		'nvim-tree/nvim-web-devicons'
+	},
+	{
+		'tpope/vim-fugitive',
+    keys = {
+      {
+        '<leader>gc',
+        '<cmd>Git commit<cr>'
+      },
+      {
+        '<leader>gd',
+        '<cmd>Git diff --cached<cr>'
+      },
+      {
+        '<leader>gs',
+        '<cmd>Git status<cr>'
+      }
+    }
 	}
 }
