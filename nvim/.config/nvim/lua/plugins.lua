a9d896ed21d5b210fe107a25e7e5ba052ceeff20
Patrick Lewis <patrick@patricklewis.org>
2023-02-21 08:55:40 -0500

    Add noice

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,5 +1,32 @@
 return {
   {
+    'folke/noice.nvim',
+    config = function()
+      require('noice').setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify'
+    }
+  },
+  {
     'folke/trouble.nvim',
     config = function()
       require('trouble').setup()
@@ -81,15 +108,15 @@ return {
     config = function()
       require('nvim-tree').setup()
     end,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
     keys = {
       {
         '<leader>t',
         '<cmd>NvimTreeToggle<cr>'
       }
     }
-  },
-  {
-    'nvim-tree/nvim-web-devicons'
   },
   {
     'nvim-treesitter/nvim-treesitter',
