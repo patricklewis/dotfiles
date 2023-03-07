1e49d9a7ba652f80f0b1d73c9fd7bca255b71a25
Patrick Lewis <patrick@patricklewis.org>
2023-03-07 13:06:15 -0500

    Add lspkind.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -37,8 +37,20 @@ return {
       end
 
       local cmp = require('cmp')
+      local lspkind = require('lspkind')
 
       cmp.setup({
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol',
+            maxwidth = 50,
+            symbol_map = { Copilot = "" },
+            ellipsis_char = '...',
+            before = function(entry, vim_item)
+              return vim_item
+            end
+          })
+        },
         snippet = {
           expand = function(args)
             vim.fn['vsnip#anonymous'](args.body)
@@ -233,6 +245,9 @@ return {
         }
       }
     end
+  },
+  {
+    'onsails/lspkind.nvim'
   },
   {
     'tpope/vim-fugitive',
