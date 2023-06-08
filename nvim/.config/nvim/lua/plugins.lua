6dabfd1bf7235d044087469de3a1ef42a51782f1
Patrick Lewis <patrick@patricklewis.org>
2023-06-08 14:58:49 -0400

    Remove lspkind

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -38,20 +38,8 @@ return {
       end
 
       local cmp = require('cmp')
-      local lspkind = require('lspkind')
 
       cmp.setup({
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol',
-            maxwidth = 50,
-            symbol_map = { Copilot = "" },
-            ellipsis_char = '...',
-            before = function(entry, vim_item)
-              return vim_item
-            end
-          })
-        },
         snippet = {
           expand = function(args)
             vim.fn['vsnip#anonymous'](args.body)
@@ -296,9 +284,6 @@ return {
     dependencies = {
       'RRethy/nvim-treesitter-endwise'
     }
-  },
-  {
-    'onsails/lspkind.nvim'
   },
   {
     'rbong/vim-flog',
