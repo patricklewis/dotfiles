80cbcc2997369b95f20a75a278d484009abbdeb2
Patrick Lewis <patrick.lewis@huntclub.com>
2023-11-15 12:07:14 -0500

    add lualine tabline

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -215,6 +215,14 @@ return {
         },
         options = {
           theme = 'dracula'
+        },
+        tabline = {
+          lualine_a = {'buffers'},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {'tabs'}
         }
       }
     end,
