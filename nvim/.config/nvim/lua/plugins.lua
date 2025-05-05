b4c27284ed38b069cf1cd888f77323ea4b9faf0c
Patrick Lewis <patrick@patricklewis.org>
2025-05-05 11:25:26 -0400

    add and configure nvim-cmp

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -49,6 +49,53 @@ return {
     end
   },
   {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip'
+    },
+    config = function()
+      local cmp = require('cmp')
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+          end
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp', group_index = 2 },
+          { name = 'vsnip', group_index = 2 }
+        }, {
+          { name = 'buffer' }
+        }),
+        window = {
+          completion = { border = 'single' },
+          documentation = { border = 'single' }
+        }
+      })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      require('lspconfig')['eslint'].setup {
+        capabilities = capabilities
+      }
+
+      require('lspconfig')['ruby_lsp'].setup {
+        capabilities = capabilities
+      }
+    end
+  },
+  {
     'ibhagwan/fzf-lua',
     config = function()
       require('fzf-lua').setup({
